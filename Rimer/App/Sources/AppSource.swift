1b76311fd7112ff046107f78852fb2e6e29f2da7
cg-yeong <ds1506027@gmail.com>
2022-10-12 16:04:11 +0900

    init: App,SceneDelegate

--- Rimer/App/Sources/AppSource.swift
+++
@@ -1,8 +0,0 @@
-//
-//  AppSource.swift
-//  ProjectDescriptionHelpers
-//
-//  Created by root0 on 2022/10/12.
-//
-
-import Foundation
---
+++ Rimer/App/Sources/SceneDelegate.swift
@@ -0,0 +1,44 @@
+//
+//  SceneDelegate.swift
+//  App
+//
+//  Created by root0 on 2022/10/12.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let scene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: scene)
+        
+        let vc = UIViewController()
+        vc.view.backgroundColor = .white
+        window.rootViewController = vc
+        
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+    
+    func sceneDidDisconnect(_ scene: UIScene) {
+        
+    }
+    
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        
+    }
+    
+    func sceneWillResignActive(_ scene: UIScene) {
+        
+    }
+    
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        
+    }
+    
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        
+    }
+}
